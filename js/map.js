bba60a2b06d66c2a892734b5a70c159800b2c489
Alexander <alexandermichaelmiranda@gmail.com>
2014-05-31 13:34:56 -0700

    Added open street map layer centered at Seattle

--- js/map.js
+++ js/map.js
@@ -1,1 +1,10 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var osm = L.tileLayer('http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors,' +
+            'tiles from <a href="http://www.opencyclemap.org/">OpenCycleMap</a>'
+    });
+
+var map = L.map('map', {
+	center: [47.6210, -122.3328],
+	zoom: 12,
+	layers: [osm]
+});
