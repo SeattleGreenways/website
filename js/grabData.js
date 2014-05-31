1df08040d0c8bc04f54e73c29be5427921063983
Alexander <alexandermichaelmiranda@gmail.com>
2014-05-31 14:51:28 -0700

    Xively JavaScript library integration working with datastreams

--- js/grabData.js
+++ js/grabData.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
 
 	xively.setKey("XVHpObbqmtxoJlYgAotjEEbMMX7ON5N91OYNUTxHv2leJPdf");
 
-	var feedID = 838532991;
+	var feedID = 838532991,
+			datastreamID = "Count";
 
-	xively.datastream.get(feedID, function(datastream) {
+	xively.datastream.get(feedID, datastreamID, function(datastream) {
 		console.log(datastream);
 	
 
-		xively.datastream.subscribe(feedID, function(event, datastream_updated) {	console.log(datastream_updated); });
+		xively.datastream.subscribe(feedID, datastreamID, function(event, datastream_updated) {	console.log(datastream_updated); });
 	
 	});
 });
